175ad82a00256c7ffe39ddc006182a37c377022f
Damilare <opeyemisamuel222@gmail.com>
2021-04-28 00:52:31 +1000

    navigation done

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,16 +1,24 @@
 import { AppProps } from "next/app";
-import "../styles/globals.css";
+import "../styles/tailwind.css";
 import axios from "axios";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Nav from "../components/Nav";
+import { useRouter } from "next/router";
 
-axios.defaults.baseURL = 'http://localhost:4000/api'
-axios.defaults.withCredentials= true
+axios.defaults.baseURL = "http://localhost:4000/api";
+axios.defaults.withCredentials = true;
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
+
+  const authRoutes = ["/register", "/login"];
+  const authRoute = authRoutes.includes(pathname);
+
   return (
     <div>
       <ToastContainer />
+      {!authRoute && <Nav />}
       <Component {...pageProps} />
     </div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,8 +2,8 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily:{
-      body: ['IBM Plex Sans']
+    fontFamily: {
+      body: ["IBM Plex Sans"],
     },
     extend: {
       colors: {
@@ -21,11 +21,26 @@ module.exports = {
       },
       spacing: {
         70: "17.5rem",
+        160: "40rem",
       },
+      container: false,
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          width: "100%",
+          marginLeft: "auto",
+          marginRight: "auto",
+          "@screen sm": { maxWidth: "640px" },
+          "@screen md": { maxWidth: "768px" },
+          "@screen lg": { maxWidth: "975px" },
+        },
+      });
+    },
+  ],
 };
